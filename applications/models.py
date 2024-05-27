b8dfa5c9b7199f4bc5bace8d6393d1937479f8c7
pprakriti5 <pprakriti5@gmail.com>
2024-05-27 17:48:07 -0400

    Add Application madel and form

--- applications/models.py
+++ applications/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Application(models.Model):
+    FULLSTACK = 'fullstack'
+    FRONTEND = 'frontend'
+    BACKEND = 'backend'
+    QA = 'qa'
+
+    EXPERTISE_CHOICES = [
+        (FULLSTACK, 'Fullstack Developer'),
+        (FRONTEND, 'Front End Developer'),
+        (BACKEND, 'Backend Developer'),
+        (QA, 'QA Engineer'),
+    ]
+
+    name = models.CharField(max_length=100)
+    experience = models.PositiveIntegerField()
+    expertise = models.CharField(max_length=50, choices=EXPERTISE_CHOICES)
+    cv = models.FileField(upload_to='cvs/')
+
+    def __str__(self):
+        return self.name
