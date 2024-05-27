9430a27732bfd18131c7507aa8afa0d9633c3de1
pprakriti5 <pprakriti5@gmail.com>
2024-05-27 17:57:27 -0400

    Create views and templates

--- applications/views.py
+++ applications/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ApplicationForm
 
-# Create your views here.
+def application_form(request):
+    if request.method == 'POST':
+        form = ApplicationForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('success')
+    else:
+        form = ApplicationForm()
+    return render(request, 'applications/application_form.html', {'form': form})
+
+def success(request):
+    return render(request, 'applications/success.html')
